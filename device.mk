802c59218923f847026b5187fa041e3cd044f3b7
Jabiyeff <cebiyevanar@gmail.com>
2021-03-03 13:41:49 +0400

    santoni: PixelExperience bring up
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -28,12 +28,10 @@ PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8996/media
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
