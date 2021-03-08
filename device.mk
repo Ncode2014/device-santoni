295247e6e222f622d553fe819d54081edb2deec7
ZeeLog <Danjel.Zammit@gmail.com>
2021-03-08 13:51:48 +0400

    santoni: Restore biometrics
    This reverts commit 8776a27c43ae1af20c2ca1ffa961c887545cc426.
    
    While we're at it, add also vintf fragment & use pragma once directive
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1-service.custom \
     libbacktrace-vendor \
     libunwind-vendor
 
