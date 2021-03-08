c3216a84fb283628eeba20da1bfdf28bb02d9c18
ZeeLog <Danjel.Zammit@gmail.com>
2021-03-08 14:41:50 +0400

    santoni: sepolicy: Update, cleanup & refactor
    * relabel fingerprint HAL
    * re-organize fingerprint rules
    * update gx_fpd rules for vndbinder access
    * other rules: minor cleanup & updates
    * suppress some denials
    * remove deprecated rules
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,7 +217,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
