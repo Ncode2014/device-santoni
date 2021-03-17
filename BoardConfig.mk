5b9cacf87acd59c068c0bfb18a0c62daab321a65
Jabiyeff <cebiyevanar@gmail.com>
2021-03-17 20:11:44 +0400

    santoni: Ignore neverallow for now (TMP)
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -217,6 +217,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
