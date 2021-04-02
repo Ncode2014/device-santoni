6d668264b8b9c198d7792a9925cbed52ed15acc5
Jabiyeff <cebiyevanar@gmail.com>
2021-04-02 21:06:58 +0400

    santoni: init: Update dalvik values
    * Taken from: https://github.com/LineageOS/android_device_xiaomi_msm8937-common/blob/lineage-16.0/init/init_msm8937.cpp
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- init/init_santoni.cpp
+++ init/init_santoni.cpp
@@ -38,7 +38,9 @@
 #include "property_service.h"
 #include "log/log.h"
 
-char const *heaptargetutilization;
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 
@@ -48,15 +50,26 @@ void check_device()
 
     sysinfo(&sys);
 
-    if (sys.totalram > 2048ull * 1024 * 1024) {
-        // from phone-xhdpi-4096-dalvik-heap.mk
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from phone-xhdpi-2048-dalvik-heap.mk
-        heaptargetutilization = "0.75";
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heapminfree = "4m";
+        heapmaxfree = "8m";
+    } else if (sys.totalram > 2048ull * 1024 * 1024) {
+        // from - phone-xxhdpi-3072-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
         heapminfree = "512k";
+	heapmaxfree = "8m";
+    } else {
+        // from - phone-xxhdpi-2048-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heapminfree = "2m";
         heapmaxfree = "8m";
    }
 }
@@ -87,10 +100,10 @@ void vendor_load_properties()
     check_device();
     set_avoid_gfxaccel_config();
 
-    property_override("dalvik.vm.heapstartsize", "8m");
-    property_override("dalvik.vm.heapgrowthlimit", "192m");
-    property_override("dalvik.vm.heapsize", "512m");
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", "0.75");
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 
