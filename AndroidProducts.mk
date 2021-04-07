f1c96b6125f81b45c9f1cfcad22814064c2d8899
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-04-07 20:05:06 +0700

    Change

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/havoc_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    havoc_santoni-userdebug \
+    havoc_santoni-user \
+    havoc_santoni-eng
